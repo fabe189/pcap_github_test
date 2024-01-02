e86f638de4d4b556b930d2eeb8d7e38306180410
Florian Haider <149242442+FlorianHaider1@users.noreply.github.com>
2024-01-02 15:57:57 +0100

    Update PCAP-Kurs-Testprojekt.py
    Spielregeln eingefügt

--- PCAP-Kurs-Testprojekt.py
+++ PCAP-Kurs-Testprojekt.py
@@ -1,4 +1,7 @@
-#Test
-
-# Noch ein test, von Ralf
-print("hallo", end='#')
+# Aufgaben:
+# 1) Spielfeld erzeugen aus ASCII oder so: 3x3 Felder
+# 2) Spielmechanik: 
+# Ein Spieler hat O, ein Spieler hat X
+# Man hat Runden, in denen man 'sein' Zeichen setzen kann
+# Ein Feld das schon belegt ist kann nicht überschrieben werden.
+# 3) Sieg = 3 gleiche Zeichen in einer Reihe: Vertikal, Horiztonal , Diagonal
